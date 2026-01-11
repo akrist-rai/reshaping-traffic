2c289adea75da0980fd390da49fc505810447025
akirst <platipuskumar@gmail.com>
2026-01-11 15:30:35 +0530

    Update metrices.py

--- utils/metrices.py
+++ utils/metrices.py
@@ -1,1 +1,15 @@
+def masked_mae(pred, true, eps=1e-5):
+    mask = (true != 0).float()
+    loss = torch.abs(pred - true)
+    return (loss * mask).sum() / (mask.sum() + eps)
+
+def masked_rmse(pred, true, eps=1e-5):
+    mask = (true != 0).float()
+    loss = (pred - true) ** 2
+    return torch.sqrt((loss * mask).sum() / (mask.sum() + eps))
+
+def masked_mape(pred, true, eps=1e-5):
+    mask = (true != 0).float()
+    loss = torch.abs((pred - true) / (true + eps))
+    return (loss * mask).sum() / (mask.sum() + eps)
 
