5861c1dd8e2c2e3187438215fa1e1d80f41d991e
akirst <platipuskumar@gmail.com>
2026-01-18 09:43:08 +0530

    Update early_stopping.py

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -12,3 +12,23 @@ class EarlyStopping:
         else:
             self.counter += 1
             return self.counter >= self.patience
+
+class EarlyStopping:
+    def __init__(self, patience=10):
+        self.patience = patience
+        self.best = float("inf")
+        self.counter = 0
+
+    def load(self, best, counter):
+        self.best = best
+        self.counter = counter
+
+    def step(self, loss):
+        if loss < self.best:
+            self.best = loss
+            self.counter = 0
+            return False
+        else:
+            self.counter += 1
+            return self.counter >= self.patience
+
